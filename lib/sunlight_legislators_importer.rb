e7c3c9ce6378f27b633972ac924d027f28357d2e
Clark Kampfe <clark.kampfe@gmail.com>
2013-10-27 15:19:11 -0500

    updated legislators importer

--- lib/sunlight_legislators_importer.rb
+++ lib/sunlight_legislators_importer.rb
@@ -1,14 +1,21 @@
 require 'csv'
+require_relative '../app/models/legislator'
 
 class SunlightLegislatorsImporter
   def self.import(filename)
     csv = CSV.new(File.open(filename), :headers => true)
+    legis_args = Hash.new("")
     csv.each do |row|
       row.each do |field, value|
-        # TODO: begin
-        raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
-        # TODO: end
+        legis_args[field] = value
       end
+      Legislator.create!(:title => legis_args[:title],
+                         :firstname => legis_args[:firstname],
+                         :lastname => legis_args[:lastname],
+                         :party => legis_args[:party],
+                         :state => legis_args[:state],
+                         :district => legis_args[:district],
+                         :twitter_id => legis_args[:twitter_id])
     end
   end
 end
