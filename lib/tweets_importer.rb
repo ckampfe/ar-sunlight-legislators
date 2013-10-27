ef8a37f002b779d0d137931f7531c9af6d1302e2
Clark Kampfe <clark.kampfe@gmail.com>
2013-10-27 15:48:15 -0500

    tweets importer version downgrade

---
+++ lib/tweets_importer.rb
@@ -0,0 +1,13 @@
+require 'twitter'
+require_relative '../app/models/tweet'
+
+
+client = Twitter.configure do |config|
+  config.consumer_key        = "3qgz4FHPzKPCW26y5Z1JDw"
+  config.consumer_secret     = "xPMEniCjQjGzLsOkma0nbXJaFU7LSMQLoHJzsnuhA"
+  config.oauth_token        = "13142292-QW6851DunKTaBqEks12BRt8nAPs2d4kB8DjiAo3Fa"
+  config.oauth_token_secret = "V8PIWSRzwNIg5cVdbqtWV20m4pAAXCt2eYXA7pzWGxYfy"
+end
+
+puts client.user('kilophoton')
+
