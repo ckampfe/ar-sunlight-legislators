9b0b7e32afec2518cfe7162a91cbe1b29f4039df
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-27 15:52:05 -0500

    correct keys, etc

--- lib/tweets_importer.rb
+++ lib/tweets_importer.rb
@@ -3,10 +3,10 @@ require_relative '../app/models/tweet'
 
 
 client = Twitter.configure do |config|
-  config.consumer_key        = "3qgz4FHPzKPCW26y5Z1JDw"
+  config.consumer_key        = "3qgz4FHPzKPCW26y5ZlJDw"
   config.consumer_secret     = "xPMEniCjQjGzLsOkma0nbXJaFU7LSMQLoHJzsnuhA"
   config.oauth_token        = "13142292-QW6851DunKTaBqEks12BRt8nAPs2d4kB8DjiAo3Fa"
-  config.oauth_token_secret = "V8PIWSRzwNIg5cVdbqtWV20m4pAAXCt2eYXA7pzWGxYfy"
+  config.oauth_token_secret = "V8PlWSRzwNIg5cVdbqtWV20m4pAAXCt2eYXA7pzWGxYfy"
 end
 
 puts client.user('kilophoton')
